cd1b946be57572e7e7c48142ac66a823e03b8cce
Jarret Minkler <jarret.minkler@gmail.com>
2019-09-14 01:19:56 +0000

    Laravel UI package, presets, event book Projector

--- app/Book.php
+++ app/Book.php
@@ -2,9 +2,37 @@
 
 namespace App;
 
+use App\Events\BookCreated;
+use App\Events\DescriptionAdded;
+use App\Events\AuthorAdded;
 use Illuminate\Database\Eloquent\Model;
+use Ramsey\Uuid\Uuid;
 
 class Book extends Model
 {
-    //
+    protected $fillable = ['isbn'];
+    
+    public static function createWithAttributes(array $attributes): Book
+    {
+        $attributes['uuid'] = (string) Uuid::uuid4();
+
+        event(new BookCreated($attributes));
+
+        return static::uuid($attributes['uuid']);
+    }
+
+    public function addDescription(string $description, string $language)
+    {
+        event(new DescriptionAdded($this->uuid, $description, $language));
+    }
+
+    public function addAuthor(string $author)
+    {
+        event(new AuthorAdded($this->uuid, $author));
+    }
+
+    public static function uuid(string $uuid): ?Book
+    {
+        return static::where('uuid', $uuid)->first();
+    }
 }
